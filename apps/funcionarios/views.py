f0ebff146e6c32860381a9a1a2fa714c0e34ab2e
João Victor Moreira <jvrodriguesmoreira@gmail.com>
2019-01-14 18:36:58 -0200

    criacao crud funcionarios

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,34 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Funcionario
 
 
-def home(request):
-    return HttpResponse('olá')
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
+
+class FuncionariosNovo(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.empresa = self.request.user.funcionario.empresa
+        obj.user = User.objects.create(username=''.join(obj.nome.split(' ')))
+        obj.save()
+        return super(FuncionariosNovo, self).form_valid(form)
+
+
+class FuncionariosEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+
+class FuncionariosDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
